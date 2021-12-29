cbafe7443008e60c294228fb86ef1ed08b30028f
chaitanya-chorawala <chorawala.chaitanya@gmail.com>
2021-12-29 19:52:07 +0530

    init

---
+++ frontend/src/components/TaskForm/EditTaskForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+const EditTaskForm = (props) => {
+    const [UserInput, setUserInput] = useState({
+        id: props.OldData.id,
+        TaskName: "",
+        Time : ""
+    });
+
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+        props.OnChangeTask(UserInput);
+        setUserInput({
+            TaskName : "",
+            Time : ""
+        });
+    };
+
+    const TaskNameHandler = (e) => {
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                TaskName : e.target.value
+            }
+        });
+    };
+
+    const TimeHandler = (e) => {
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                Time : new Date(e.target.value).toLocaleDateString()
+            }
+        });
+        
+    };
+
+    return(
+        <div className="col-md-10">
+            <div className="card bg-muted">
+                <div className="card-header">
+                    <h4>Change task</h4>    
+                </div>
+                <div className="card-body bg-light">        
+                    <form onSubmit={SubmitHandler} className="form">
+                        <div className="row">                            
+                            <div className="form-group col-md-5">                
+                                <input type="text" name="txtTask" onChange={TaskNameHandler} placeholder="Task" className="form-control" ></input>  
+
+                                {/* value={props.OldData.TaskName} */}
+                            </div>
+                            <div className="form-group col-md-5">                
+                                <input type="date" name="dtTime" onChange={TimeHandler} placeholder="Task" className="form-control"></input>
+
+                                {/* value={new Date(props.OldData.Time).toISOString().slice(0, 10)} */}
+                            </div>
+                            <div className="form-group col-md-2">               
+                                <div className="btn-group">
+                                    <button type="submit" name="btnSubmit" className="btn  btn-primary">Save</button>
+                                    <button type="button" name="btnCancel" className="btn  btn-danger" onClick={props.OnCancel}>Cancel</button>
+                                </div> 
+                            </div>
+                        </div>
+                    </form>                   
+                </div>
+            </div>
+        </div>
+    );
+};
+export default EditTaskForm;
